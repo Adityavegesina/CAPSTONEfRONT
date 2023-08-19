37e84896276fef7c2e0f8a00d191de2d269a4adf
Diksha Rani <83601273+Dik17@users.noreply.github.com>
2023-08-19 15:33:28 +0530

    initial commit

---
+++ src/Doctor/Mainpage/Mainpage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+import Sidebar from '../../Hospital/Dashboard/Reception/Sidebar';
+const Mainpage = () => {
+  return (
+    <div>
+    <Sidebar/>
+        <img src='https://media.istockphoto.com/id/1204177269/photo/portrait-of-multi-cultural-medical-team-standing-in-hospital-corridor.jpg?s=612x612&w=0&k=20&c=PR3ulMitMJnrYRpe12C95HWnLWvU15NP6QcLtxEQYJQ=' style={{minWidth:'100%'}}/>
+      </div>
+  );
+};
+
+export default Mainpage;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM  from "react-dom";
+import App from "./App";
+import Register from "./LandingPageComponent/Register/Register";
+import Login from "./LandingPageComponent/Login/Login";
+import Dashboard from "./Hospital/Dashboard/Dashboard";
+import Reception from "./Hospital/Dashboard/Reception/Reception";
+import Mainpage from "./Doctor/Mainpage/Mainpage";
+import {
+    createBrowserRouter,
+    RouterProvider,
+  } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <App/>
+    },
+    {
+      path: "/login",
+      element: <Login/>
+    },
+    {
+      path: "/register",
+      element: <Register/>
+    },{
+      path:"/hospital",
+      element:<Dashboard/>
+    },
+    {
+      path:"/reception",
+      element:<Reception/>
+    },
+    {
+      path:"/doctor",
+      element:<Mainpage/>
+    }])
+  const root=ReactDOM.createRoot(document.getElementById('root'));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<RouterProvider router={router}/>);
